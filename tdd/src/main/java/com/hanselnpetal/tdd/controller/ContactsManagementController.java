d043974e1f47ccc43703b9601c73dbc451a353dc
khwilo <khwilowatai@gmail.com>
2019-08-20 22:26:23 +0300

    Add a contacts management controller

--- tdd/src/main/java/com/hanselnpetal/tdd/controller/ContactsManagementController.java
+++ tdd/src/main/java/com/hanselnpetal/tdd/controller/ContactsManagementController.java
@@ -1,4 +1,32 @@
 package com.hanselnpetal.tdd.controller;
 
+import com.hanselnpetal.tdd.domain.CustomerContact;
+import com.hanselnpetal.tdd.service.ContactsManagementService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/addContact")
 public class ContactsManagementController {
+
+    private ContactsManagementService contactsManagementService;
+
+    @Autowired
+    public ContactsManagementController(ContactsManagementService contactsManagementService) {
+        this.contactsManagementService = contactsManagementService;
+    }
+
+    @PostMapping
+    public String processAddContactSubmit(@RequestBody CustomerContact customerContact) {
+        CustomerContact newContact = contactsManagementService.add(customerContact);
+
+        if (newContact != null) {
+            return "success";
+        }
+        return "failure";
+    }
 }
