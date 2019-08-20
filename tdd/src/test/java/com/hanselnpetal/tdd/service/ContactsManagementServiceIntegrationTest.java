402cdcafb240a62a19d4d980c3eb1cb83ba6967b
khwilo <khwilowatai@gmail.com>
2019-08-20 17:46:43 +0300

    Add a minimal integration test for contacts

---
+++ tdd/src/test/java/com/hanselnpetal/tdd/service/ContactsManagementServiceIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.hanselnpetal.tdd.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.hanselnpetal.tdd.domain.CustomerContact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class ContactsManagementServiceIntegrationTest {
+
+    @Autowired
+    private ContactsManagementService contactsManagementService;
+
+    @Test
+    public void testAddContactHappyPath() {
+        // Create a contact
+        CustomerContact aContact = new CustomerContact();
+        aContact.setFirstName("Jenny");
+        aContact.setLastName("Johnson");
+
+        // Test adding the contact
+        CustomerContact newContact = contactsManagementService.add(aContact);
+
+        // Verify the addition
+        assertNotNull(newContact);
+        assertNotNull(newContact.getId());
+        assertEquals("Jenny", newContact.getFirstName());
+    }
+}
