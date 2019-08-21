5f6509887e988e49ae28abdf67434c804ed80fd7
khwilo <khwilowatai@gmail.com>
2019-08-21 10:42:33 +0300

    Define a method to find a customer contact by email

--- tdd/src/main/java/com/hanselnpetal/tdd/data/repos/CustomerContactRepository.java
+++ tdd/src/main/java/com/hanselnpetal/tdd/data/repos/CustomerContactRepository.java
@@ -5,4 +5,6 @@ import com.hanselnpetal.tdd.domain.CustomerContact;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerContactRepository extends CrudRepository<CustomerContact, Long> {
+
+    public CustomerContact findByEmail(String email);
 }
